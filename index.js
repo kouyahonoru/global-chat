b6ff5a2f58ee7c7af7175dd1968c542be6fbd47e
mouse484 <escmouse484@gmail.com>
2019-01-09 19:43:10 +0900

    eslintルールの設定とそれに伴う修正

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log(`Botを起動しました ${client.user.tag}!`);
+    console.log(`Botを起動しました ${client.user.tag}!`);
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
-  }
+    if (msg.content === "ping") {
+        msg.reply("Pong!");
+    }
 });
 
 client.login(process.env.TOKEN);
