be0638bebce9f9cfb3c4f812cd23c6f9ce8d71d4
mouse484 <escmouse484@gmail.com>
2019-01-10 10:30:20 +0900

    fixed #3

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-require("http").createServer().listen(3000);
+require("http")
+  .createServer()
+  .listen(3000);
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-    console.log(`Botを起動しました ${client.user.tag}!`);
+  console.log(`Botを起動しました ${client.user.tag}!`);
 });
 
 client.on("message", message => {
-    if (!message.channel.name === "esc-global-chat") return;
-    
-    const global_msg = message.content;
+  if (message.author.bot) return;
 
-    client.channels.forEach((channel)=>{
-        if(channel.name === "esc-global-chat"){
-            message.channel.send(global_msg);
-        }
-    });
+  if (!message.channel.name === "esc-global-chat") return;
+
+  const global_msg = message.content;
+
+  client.channels.forEach(channel => {
+    if (channel.name === "esc-global-chat") {
+      message.channel.send(global_msg);
+    }
+  });
 });
 
 client.login(process.env.TOKEN);
