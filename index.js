fc1c21ab4842a5c284aa2d333510f6194a8fea70
mouse484 <escmouse484@gmail.com>
2019-01-10 14:15:19 +0900

    webhookを使用できるように変更

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-require("http")
-  .createServer()
-  .listen(3000);
+require("http").createServer().listen(3000);
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -14,13 +12,42 @@ client.on("message", message => {
 
   if (!message.channel.name === "esc-global-chat") return;
 
-  const global_msg = message.content;
+  if(message.mentions.users.first()){
+      message.reply("グローバルチャットでのメンションは禁止です");
+      return;
+  }
 
-  client.channels.forEach(channel => {
+  const global_msg = message.content;
+  const hook_option = {
+    username: message.author.tag,
+    avatarURL: message.author.avatarURL,
+    disableEveryone: true
+  };
+
+  client.channels.forEach(async channel => {
     if (channel.name === "esc-global-chat") {
-      message.channel.send(global_msg);
+      try {
+        const hooks = await channel.fetchWebhooks();
+
+        if (hooks.size === 0) {
+          if (channel.id === message.channel.id) {
+            channel.createWebhook("global-chat").then(hook => {
+              message.channel.send(
+                "グローバルチャットが利用できるようになりました！"
+              );
+              hook.send(global_msg,hook_option);
+            });
+          }
+        } else {
+          hooks.first().send(global_msg,hook_option);
+        }
+        message.delete(100);
+      } catch (error) {
+        message.reply("WebHookの管理権限を付与してください");
+      }
     }
   });
 });
+
 
 client.login(process.env.TOKEN);
