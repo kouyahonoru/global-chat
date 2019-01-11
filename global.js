21b8afc97edfdd738c0fbce00e94d0d8b5ede08a
mouse484 <escmouse484@gmail.com>
2019-01-11 17:40:44 +0900

    グローバルチャットのメイン部分を分離

---
+++ global.js
@@ -0,0 +1,53 @@
+module.exports.run = (client, message) => {
+    if (!message.guild.me.hasPermission("MANAGE_WEBHOOKS")) {
+        message.reply("WebHooksの管理権限を付与してください");
+        return;
+    }
+
+    if (message.mentions.users.first()) {
+        message.reply("グローバルチャットでのメンションは禁止です");
+        return;
+    }
+
+    const global_msg = message.content;
+    const hook_option = {
+        username: message.author.tag,
+        avatarURL: message.author.avatarURL,
+        disableEveryone: true,
+    };
+
+    client.channels.forEach(async channel => {
+        if (channel.name === "esc-global-chat") {
+            try {
+                if (!channel.guild.me.hasPermission("MANAGE_WEBHOOKS")) return;
+
+                const hooks = await channel.fetchWebhooks();
+
+                if (hooks.size === 0) {
+                    if (channel.id === message.channel.id) {
+                        channel.createWebhook("global-chat").then(hook => {
+                            message.channel.send(
+                                "グローバルチャットが利用できるようになりました！"
+                            );
+                            hook.send(global_msg, hook_option);
+                        });
+                    }
+                } else {
+                    hooks.first().send(global_msg, hook_option);
+                }
+            } catch (error) {
+                message.reply(
+                    "エラーが発生しました\nもう一度送信しても解決しない場合は`mouse#2240`まで"
+                );
+            }
+        }
+    });
+
+    message
+        .delete(2000)
+        .catch(() =>
+            message.reply(
+                "メッセージの管理権限を付与してください\nそれでも解決しない場合は`mouse#2240`まで"
+            )
+        );
+};
--- index.js
+++ index.js
@@ -1,70 +1,22 @@
 require("http")
-  .createServer()
-  .listen(3000);
+    .createServer()
+    .listen(3000);
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log(`Botを起動しました ${client.user.tag}!`);
+    console.log(`Botを起動しました ${client.user.tag}!`);
 });
 
+const global_chat = require("./global.js");
+
 client.on("message", message => {
-  if (message.author.bot || !message.guild) return;
+    if (message.author.bot || !message.guild) return;
 
-  if (!message.channel.name === "esc-global-chat") return;
-
-  if (!message.guild.me.hasPermission("MANAGE_WEBHOOKS")) {
-    message.reply("WebHooksの管理権限を付与してください");
-    return;
-  }
-
-  if (message.mentions.users.first()) {
-    message.reply("グローバルチャットでのメンションは禁止です");
-    return;
-  }
-
-  const global_msg = message.content;
-  const hook_option = {
-    username: message.author.tag,
-    avatarURL: message.author.avatarURL,
-    disableEveryone: true
-  };
-
-  message
-    .delete(2000)
-    .catch(() =>
-      message.reply(
-        "メッセージの管理権限を付与してください\nそれでも解決しない場合は`mouse#2240`まで"
-      )
-    );
-
-  client.channels.forEach(async channel => {
-    if (channel.name === "esc-global-chat") {
-      try {
-        if (!channel.guild.me.hasPermission("MANAGE_WEBHOOKS")) return;
-        const hooks = await channel.fetchWebhooks();
-
-        if (hooks.size === 0) {
-          if (channel.id === message.channel.id) {
-            channel.createWebhook("global-chat").then(hook => {
-              message.channel.send(
-                "グローバルチャットが利用できるようになりました！"
-              );
-              hook.send(global_msg, hook_option);
-            });
-          }
-        } else {
-          hooks.first().send(global_msg, hook_option);
-        }
-      } catch (error) {
-        message.reply(
-          "エラーが発生しました\nもう一度送信しても解決しない場合は`mouse#2240`まで" +
-            error
-        );
-      }
+    if (message.channel.name === "esc-global-chat") {
+        global_chat.run(client, message);
     }
-  });
 });
 
 client.login(process.env.TOKEN);
