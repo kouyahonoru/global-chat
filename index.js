515213825b35b709d51f5fa0cc398fa774400a9f
氷河 <9rasjk2wxpa6@gmail.com>
2019-01-12 17:14:21 +0900

    修正

--- index.js
+++ index.js
@@ -7,36 +7,32 @@ client.on("ready", () => {
     console.log(`Botを起動しました ${client.user.tag}!`);
 });
 
+const global_chat = require("./global.js");
+
 client.on("message", message => {
-    if(message.author.bot)return;
-    if (!message.channel.name === "esc-global-chat") return;
+    if (message.author.bot || !message.guild) return;
 
     if(message.content === "g!help"){
-        let embed = {
-            "title":`${client.user.tag}の使い方`,
-            "fields":[{
-            "name":"利用規約",
-            "value":"正しくご利用をお願いします",
-            "name":"接続方法",
-            "value":"`esc-global-chat`を作成するだけです！",
-            "name":"必要権限",
-            "value":"webhook、メッセージの編集権限をください！",
-            "name":"バグ・質問",
-            "value":"準備中",
-            "name":"作成者",
-            "value":"Esc",
-            }]
-            };
-            message.reply({embed});
+    let embed = {
+    "title":`${client.user.tag}の使い方`,
+    "fields":[{
+    "name":"利用規約",
+    "value":"正しくご利用をお願いします",
+    "name":"接続方法",
+    "value":"`esc-global-chat`を作成するだけです！",
+    "name":"必要権限",
+    "value":"webhook、メッセージの編集権限をください！",
+    "name":"バグ・質問",
+    "value":"準備中",
+    "name":"作成者",
+    "value":"Esc",
+    }]
+    };
+    message.reply({embed});
+
+    if (message.channel.name === "esc-global-chat") {
+        global_chat.run(client, message);
     }
-
-    const global_msg = message.content;
-
-    client.channels.forEach((channel)=>{
-        if(channel.name === "esc-global-chat"){
-            message.channel.send(global_msg);
-        }
-    });
 });
 
 client.login(process.env.TOKEN);
