a53b2a360c38490caeb74d4aeae67f21871f96a7
氷河 <9rasjk2wxpa6@gmail.com>
2019-01-12 18:34:31 +0900

    komitto

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require("http").createServer().listen(3000);
+require("http")
+    .createServer()
+    .listen(3000);
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -7,17 +9,40 @@ client.on("ready", () => {
     console.log(`Botを起動しました ${client.user.tag}!`);
 });
 
+const global_chat = require("./global.js");
+
 client.on("message", message => {
-    if(message.author.bot)return;
-    if (!message.channel.name === "esc-global-chat") return;
+    if (message.author.bot || !message.guild) return;
 
-    const global_msg = message.content;
+    if (message.content === "g!help") {
+        const embed = {
+            username: `${client.user.username}の使い方`,
+            description: "正しく利用してください",
+            fields: [
+                {
+                    name: "接続方法",
+                    value: "`esc-global-chat`を作成するだけです！",
+                },
+                {
+                    name: "必要権限",
+                    value: "webhook、メッセージの編集権限をください！",
+                },
+                {
+                    name: "バグ・質問",
+                    value: "[こちら](https://discord.gg/7dGssZn)にお願いします",
+                },
+                {
+                    name: "作成者",
+                    value: "[Esc]™ グループ",
+                },
+            ],
+        };
+        message.channel.send({ embed });
+    }
 
-    client.channels.forEach((channel)=>{
-        if(channel.name === "esc-global-chat"){
-            message.channel.send(global_msg);
-        }
-    });
+    if (message.channel.name === "esc-global-chat") {
+        global_chat.run(client, message);
+    }
 });
 
 client.login(process.env.TOKEN);
