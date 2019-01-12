bf4be22689e5025cb58907e3c8fb6e79364ce8e6
氷河 <9rasjk2wxpa6@gmail.com>
2019-01-12 17:56:01 +0900

    こうかな

--- index.js
+++ index.js
@@ -21,18 +21,26 @@ client.on("message", message => {
                 {
                     name: "利用規約",
                     value: "正しくご利用をお願いします",
+                },
+                {
                     name: "接続方法",
                     value: "`esc-global-chat`を作成するだけです！",
+                },
+                {
                     name: "必要権限",
                     value: "webhook、メッセージの編集権限をください！",
+                },
+                {
                     name: "バグ・質問",
                     value: "準備中",
+                },
+                {
                     name: "作成者",
                     value: "Esc",
                 },
             ],
         };
-        message.reply({ embed });
+        message.channel.send(embed);
     }
 
     if (message.channel.name === "esc-global-chat") {
