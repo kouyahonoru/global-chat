f89a6a5d27a3cadad83ebaff799b378588b9208b
氷河 <9rasjk2wxpa6@gmail.com>
2019-01-12 17:03:34 +0900

    コミットー

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ client.on("message", message => {
     if(message.author.bot)return;
     if (!message.channel.name === "esc-global-chat") return;
 
+    if(message.content === "g!help"){
+        let embed = {
+            "title":`${client.user.tag}の使い方`,
+            "fields":[{
+            "name":"利用規約",
+            "value":"正しくご利用をお願いします",
+            "name":"接続方法",
+            "value":"`esc-global-chat`を作成するだけです！",
+            "name":"必要権限",
+            "value":"webhook、メッセージの編集権限をください！",
+            "name":"バグ・質問",
+            "value":"準備中",
+            "name":"作成者",
+            "value":"Esc",
+            }]
+            };
+            message.reply({embed});
+    }
+
     const global_msg = message.content;
 
     client.channels.forEach((channel)=>{
